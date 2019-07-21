f8b30a3518e869ef38358863a5bd408dc3c9b627
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-07-21 11:48:23 -0500

    added ask function

---
+++ bamazonCustomer.js
@@ -0,0 +1,44 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon"
+});
+
+connection.connect (function (err){
+    if (err) throw err;
+    console.log("Connected")
+    displayProducts ();
+});
+
+//show all products
+function displayProducts () {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        console.table (res);
+        ask();
+    });
+}
+
+function ask(){
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Enter ID of the product you would like to purchase."
+        }
+    ]).then (function (answer){
+        console.log(answer.ID);
+        var item = connection.query('SELECT * FROM products WHERE id= ?', [answer.ID], function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            howMany(res);
+    })
+});
+}
+
+
